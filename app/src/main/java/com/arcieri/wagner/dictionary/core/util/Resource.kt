06e1a6c938758010e8a374c8dd2cf30bfffb6c32
Wagner Arcieri <84655093+WagnerArcieri@users.noreply.github.com>
2022-07-01 08:19:21 -0300

    created Resource utils

---
+++ app/src/main/java/com/arcieri/wagner/dictionary/core/util/Resource.kt
@@ -0,0 +1,7 @@
+package com.arcieri.wagner.dictionary.core.util
+
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T?) : Resource<T>(data)
+    class Loading<T>(data: T? = null) : Resource<T>()
+    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+}
