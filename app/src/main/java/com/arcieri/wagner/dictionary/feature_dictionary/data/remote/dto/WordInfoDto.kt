90fa314fe348a5199dcf2e922a6eacc3aa0b1e58
Wagner Arcieri <84655093+WagnerArcieri@users.noreply.github.com>
2022-07-01 08:21:13 -0300

    changed name to ...Entity()

--- app/src/main/java/com/arcieri/wagner/dictionary/feature_dictionary/data/remote/dto/WordInfoDto.kt
+++ app/src/main/java/com/arcieri/wagner/dictionary/feature_dictionary/data/remote/dto/WordInfoDto.kt
@@ -1,5 +1,6 @@
 package com.arcieri.wagner.dictionary.feature_dictionary.data.remote.dto
 
+import com.arcieri.wagner.dictionary.feature_dictionary.data.local.entity.WordInfoEntity
 import com.arcieri.wagner.dictionary.feature_dictionary.domain.model.WordInfo
 
 data class WordInfoDto(
@@ -9,12 +10,13 @@ data class WordInfoDto(
     val sourceUrls: List<String>,
     val word: String
 ) {
-    fun toWordInfo(): WordInfo {
-        return WordInfo(
+    fun toWordInfoEntity(): WordInfoEntity {
+        return WordInfoEntity(
         meanings = meanings.map { it.toMeaning() },
             phonetic = phonetic,
             sourceUrls = sourceUrls,
             word = word
         )
     }
+
 }
